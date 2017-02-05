53124c78a92228db411c66d394909b419156aafb
Philipp Schmitt <philipp@schmitt.co>
2017-02-05 10:20:01 +0100

    Implement a few shortcut functions + Default to httpie if available

--- hass.sh
+++ hass.sh
@@ -6,10 +6,14 @@ HASS_URL="https://$(awk '/base_url/ { print $2;exit }' ../config/secrets.yaml)"
 HASS_PASSWORD=$(awk '/http_password/ { print $2;exit }' ../config/secrets.yaml)
 
 usage() {
-    echo "Usage: $(basename "$0") get|post API_ENDPOINT"
+    echo -e "Usage: $(basename "$0") ACTION API_ENDPOINT [DATA]\n"
+    echo -e "ACTION: raw|script|event\n"
+    echo -e "- raw [GET|POST] API_ENDPOINT [DATA]"
+    echo -e "- script SCRIPT_NAME [DATA]"
+    echo -e "- event EVENT_NAME"
 }
 
-__rq() {
+__rq_curl() {
     if [[ -n "$3" ]]
     then
         local json_data="-d $3"
@@ -20,6 +24,21 @@ __rq() {
         "$json_data" \
         "${HASS_URL}/api/$2"
     echo
+}
+
+__rq_httpie() {
+    http -j -p b "$1" "${HASS_URL}/api/$2" \
+        "X-HA-ACCESS:$HASS_PASSWORD" $3
+}
+
+__rq() {
+    # Use httpie by default (if available)
+    if command -v http >/dev/null 2>&1
+    then
+        __rq_httpie $*
+    else
+        __rq_curl $*
+    fi
 }
 
 rq() {
@@ -37,12 +56,41 @@ rq_post() {
     rq POST "$1" "$2"
 }
 
+call_script() {
+    rq POST "services/script/$1" "$2"
+}
+
+fire_event() {
+    rq POST "events/$1"
+}
+
+notify() {
+    rq POST "services/notify/$1" "$2"
+}
+
 case "$1" in
-    g|get|GET|G)
-        rq_get "$2" "$3"
+    r|raw)
+        case "$2" in
+            g|get|GET|G)
+                rq_get "$3" "$4"
+                ;;
+            p|post|POST|p)
+                rq_post "$3" "$4"
+                ;;
+            *)
+                usage
+                exit 2
+                ;;
+        esac
         ;;
-    p|post|POST|p)
-        rq_post "$2" "$3"
+    s|script)
+        call_script "$2" "$3"
+        ;;
+    e|event)
+        fire_event "$2"
+        ;;
+    n|notify)
+        notify "$2" "$3"
         ;;
     *)
         usage
